51cf1e93d13faa7001b36870c50bdae59f0e0f97
Gabulin <andrew_bd_github@mail.ru>
2023-08-28 09:06:07 +0700

    fix:const

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -1,6 +1,6 @@
 export const REGEX_EMAIL = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-//export const BASE_URL = "https://gn.movies-explorer.api.nomoreparties.co";
-export const BASE_URL = 'http://localhost:3000'
+export const BASE_URL = "https://gn.movies-explorer.api.nomoreparties.co";
+//export const BASE_URL = 'http://localhost:3000'
 export const MOVIE_URL = "https://api.nomoreparties.co"; 
 
 export const HEADERS = (token) => ({
@@ -10,5 +10,5 @@ export const HEADERS = (token) => ({
 
 export const MAX_SHORT_FILM_DURATION = 60;
 export const MOBILE_WIDTH = 768;
-export const TABLET_WIDTH = 1000;
+export const TABLET_WIDTH = 1001;
 export const DESKTOP_WIDTH = 1280;
